9ec5668e48343fb3038c8bea65355e129cf65b27
Veronica Andersen <veronica.andersen@se.experis.com>
2022-08-11 14:47:57 +0200

    Chore: Added chinook spring boot.

---
+++ src/main/java/com/example/chinookjava/ChinookJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chinookjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChinookJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/chinookjava/ChinookJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinookjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
