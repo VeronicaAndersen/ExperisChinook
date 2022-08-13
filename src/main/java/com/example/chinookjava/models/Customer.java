d758f4b96df96bd6595d9577cb405da668cd358e
Veronica Andersen <v.andersen98@gmail.com>
2022-08-13 18:03:37 +0200

    Refactor: Cleaned up code and refactor the printouts.

--- src/main/java/com/example/chinookjava/models/Customer.java
+++ src/main/java/com/example/chinookjava/models/Customer.java
@@ -3,5 +3,16 @@ package com.example.chinookjava.models;
 Id, first name, last name, country, postal code, phone number and email.*/
 public record Customer(int customer_id, String first_name, String last_name, String country, String postal_code, String phone, String email) {
 
+	public String getCustomerInformation() {
+        StringBuilder customerInfo = new StringBuilder();
+        customerInfo.append(customer_id + " ");
+        customerInfo.append(first_name + " ");
+        customerInfo.append(last_name + " ");
+        customerInfo.append(country + " ");
+        customerInfo.append(postal_code + " ");
+        customerInfo.append(phone + " ");
+        customerInfo.append(email + " ");
+        return customerInfo.toString();
+    }
 
 }
--- src/main/java/com/example/chinookjava/runners/CustomerRunner.java
+++ src/main/java/com/example/chinookjava/runners/CustomerRunner.java
@@ -6,43 +6,40 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class CustomerRunner implements ApplicationRunner {
-  private final CustomerRepository customerRepository;
+	private final CustomerRepository customerRepository;
 
-  public CustomerRunner(CustomerRepository customerRepository) {
-    this.customerRepository = customerRepository;
-  }
+	public CustomerRunner(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
-  @Override
-  public void run(ApplicationArguments args) throws Exception {
-    /*Prints out all costumers*/
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		/*Insert a new customer.*/
+//		customerRepository.insert(new Customer(0, "Adrian", "Mattsson", "Sweden", "54134", "824674-23423", "adrian@mattsson.com"));
 
-    List<Customer> customerList = customerRepository.findAll();
-    System.out.println(customerList);
-    List<Customer> customerListLimit = customerRepository.findAll(2, 12);
-    System.out.println(customerListLimit);
+		/*Update an existing customer*/
+//		customerRepository.update(new Customer(2, "Oliver", "Dahlqvist", "Sweden", "54144", "224256-2343", "oliver@dalhqvist.com"));
 
+		/*Prints out all customers.*/
+		System.out.println("\n_______________________Find all customers._______________________");
+		List<Customer> customerList = customerRepository.findAll();
+		customerList.forEach(customer -> System.out.println(customer.getCustomerInformation()));
 
+		/*Prints out all customers with limit & offset.*/
+		System.out.println("\n_______________________Find all customers with limits & offsets._______________________");
+		List<Customer> customerListLimit = customerRepository.findAll(2, 12);
+		customerListLimit.forEach(customer -> System.out.println(customer.getCustomerInformation()));
 
+		/*Prints out customer by first name*/
+		System.out.println("\n_______________________Find customer by name._______________________");
+		Customer customerName = customerRepository.findByName("Adrian");
+		System.out.println("\n" + customerName.getCustomerInformation());
 
-//    customerRepository.insert(new Customer(0, "Adrian", "Mattsson", "Sweden", "54134", "824674-23423", "adrian@mattsson.com"));
-
-    Customer customerName = customerRepository.findByName("Adrian");
-    System.out.println(customerName.customer_id() + " " + customerName.first_name() + " " +
-            customerName.last_name() + " " + customerName.country() + " " +
-            customerName.postal_code() + " " + customerName.phone() + " " + customerName.email());
-
-    customerRepository.update(new Customer(2, "Sona", "Dahlqvist", "Sweden", "54144", "224256-2343", "oliver@dalhqvist.com"));
-
-
-    Customer customer = customerRepository.findById(2);
-    System.out.println(customer.customer_id() + " " + customer.first_name() + " " +
-            customer.last_name() + " " + customer.country() + " " +
-            customer.postal_code() + " " + customer.phone() + " " + customer.email());
-
-  }
+	}
 }
