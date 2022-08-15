c2da73a6eb829523d4bdfbe6771ebd11b00bd173
Veronica Andersen <veronica.andersen@se.experis.com>
2022-08-15 13:06:52 +0200

    Docs: Added comments in CustomerRepositoryImpl.

--- src/main/java/com/example/chinookjava/repository/customer/CustomerRepositoryImpl.java
+++ src/main/java/com/example/chinookjava/repository/customer/CustomerRepositoryImpl.java
@@ -12,232 +12,236 @@ import java.util.List;
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
 
-	private String url;
-	private String username;
-	private String password;
-	private PreparedStatement preparedStatement;
+  private String url;
+  private String username;
+  private String password;
+  private PreparedStatement preparedStatement;
 
-	public CustomerRepositoryImpl(@Value("${spring.datasource.url}") String url,
-			@Value("${spring.datasource.username}") String username,
-			@Value("${spring.datasource.password}") String password) {
-		this.url = url;
-		this.username = username;
-		this.password = password;
-	}
+  public CustomerRepositoryImpl(@Value("${spring.datasource.url}") String url,
+                                @Value("${spring.datasource.username}") String username,
+                                @Value("${spring.datasource.password}") String password) {
+    this.url = url;
+    this.username = username;
+    this.password = password;
+  }
 
-	@Override
-	public List<Customer> findAll() {
-		Customer customer = null;
-		List<Customer> customerList = new ArrayList<>();
-		String sql = "SELECT * FROM customer ORDER BY customer_id ASC";
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
+  /* 1. Read all the customers in the database, this display their: Id, first name, last name, country, postal code,
+  phone number and email.*/
+  @Override
+  public List<Customer> findAll() {
+    Customer customer = null;
+    List<Customer> customerList = new ArrayList<>();
+    String sql = "SELECT * FROM customer ORDER BY customer_id ASC";
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
 
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				customer = new Customer(
-						resultSet.getInt("customer_id"),
-						resultSet.getString("first_name"),
-						resultSet.getString("last_name"),
-						resultSet.getString("country"),
-						resultSet.getString("postal_code"),
-						resultSet.getString("phone"),
-						resultSet.getString("email")
-						);
-				customerList.add(customer);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return customerList;
-	}
-	
-	@Override
-	public List<Customer> findAll(int rowLimit, int offsetLimit) {
-		Customer customer = null;
-		List<Customer> customerList = new ArrayList<>();
-		String sql = "SELECT * FROM customer LIMIT ? OFFSET ?";
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
-			preparedStatement.setInt(1, rowLimit);
-			preparedStatement.setInt(2, offsetLimit);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				customer = new Customer(
-						resultSet.getInt("customer_id"),
-						resultSet.getString("first_name"),
-						resultSet.getString("last_name"),
-						resultSet.getString("country"),
-						resultSet.getString("postal_code"),
-						resultSet.getString("phone"),
-						resultSet.getString("email")
-						);
-				customerList.add(customer);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return customerList;
-	}
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        customer = new Customer(
+                resultSet.getInt("customer_id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("country"),
+                resultSet.getString("postal_code"),
+                resultSet.getString("phone"),
+                resultSet.getString("email")
+        );
+        customerList.add(customer);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return customerList;
+  }
 
-	@Override
-	public Customer findById(Integer id) {
-		Customer customer = null;
-		String sql = "SELECT * FROM customer WHERE customer_id = ?";
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
-			preparedStatement.setInt(1, id);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				customer = new Customer(
-						resultSet.getInt("customer_id"),
-						resultSet.getString("first_name"),
-						resultSet.getString("last_name"),
-						resultSet.getString("country"),
-						resultSet.getString("postal_code"),
-						resultSet.getString("phone"),
-						resultSet.getString("email")
-						);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return customer;
-	}
+  /* 4. Read all the customers in the database, this display their: Id, first name, last name, country, postal code,
+phone number and email. Whit limit & offsets*/
+  @Override
+  public List<Customer> findAll(int rowLimit, int offsetLimit) {
+    Customer customer = null;
+    List<Customer> customerList = new ArrayList<>();
+    String sql = "SELECT * FROM customer LIMIT ? OFFSET ?";
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
+      preparedStatement.setInt(1, rowLimit);
+      preparedStatement.setInt(2, offsetLimit);
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        customer = new Customer(
+                resultSet.getInt("customer_id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("country"),
+                resultSet.getString("postal_code"),
+                resultSet.getString("phone"),
+                resultSet.getString("email")
+        );
+        customerList.add(customer);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return customerList;
+  }
 
-	@Override
-	public Customer findByName(String name) {
-		Customer customer = null;
-		String sql = "SELECT * FROM customer WHERE first_name LIKE ? ORDER BY customer_id ASC";
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
-			preparedStatement.setString(1, name);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				customer = new Customer(
-						resultSet.getInt("customer_id"),
-						resultSet.getString("first_name"),
-						resultSet.getString("last_name"),
-						resultSet.getString("country"),
-						resultSet.getString("postal_code"),
-						resultSet.getString("phone"),
-						resultSet.getString("email")
-						);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return customer;
-	}
+  /* 2. Read a specific customer from the database (by Id), this display their: Id, first name, last name, country, postal code,
+  phone number and email.*/
+  @Override
+  public Customer findById(Integer id) {
+    Customer customer = null;
+    String sql = "SELECT * FROM customer WHERE customer_id = ?";
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
+      preparedStatement.setInt(1, id);
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        customer = new Customer(
+                resultSet.getInt("customer_id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("country"),
+                resultSet.getString("postal_code"),
+                resultSet.getString("phone"),
+                resultSet.getString("email")
+        );
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return customer;
+  }
 
-	@Override
-	public void insert(Customer customer) {
-		String sql = "INSERT INTO customer (first_name, last_name, country, postal_code, phone, email) VALUES (?,?,?,?,?,?)";
+  /* 3. Read a specific customer by name.*/
+  @Override
+  public Customer findByName(String name) {
+    Customer customer = null;
+    String sql = "SELECT * FROM customer WHERE first_name LIKE ? ORDER BY customer_id ASC";
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
+      preparedStatement.setString(1, name);
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        customer = new Customer(
+                resultSet.getInt("customer_id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("country"),
+                resultSet.getString("postal_code"),
+                resultSet.getString("phone"),
+                resultSet.getString("email")
+        );
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return customer;
+  }
 
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
-			preparedStatement.setString(1, customer.first_name());
-			preparedStatement.setString(2, customer.last_name());
-			preparedStatement.setString(3, customer.country());
-			preparedStatement.setString(4, customer.postal_code());
-			preparedStatement.setString(5, customer.phone());
-			preparedStatement.setString(6, customer.email());
-			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+  /* 5. Add a new customer to the database.*/
+  @Override
+  public void insert(Customer customer) {
+    String sql = "INSERT INTO customer (first_name, last_name, country, postal_code, phone, email) VALUES (?,?,?,?,?,?)";
 
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
+      preparedStatement.setString(1, customer.first_name());
+      preparedStatement.setString(2, customer.last_name());
+      preparedStatement.setString(3, customer.country());
+      preparedStatement.setString(4, customer.postal_code());
+      preparedStatement.setString(5, customer.phone());
+      preparedStatement.setString(6, customer.email());
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 
-	@Override
-	public int update(Customer customer) {
-		String sql = "UPDATE customer SET first_name = ?, " +
-										"last_name = ?, " +
-										"country = ?, " +
-										"postal_code = ?, " +
-										"phone = ?, " +
-										"email = ? " +
-										"WHERE customer_id = ?";
-		int rowAffected = 0;
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
-			preparedStatement.setString(1, customer.first_name());
-			preparedStatement.setString(2, customer.last_name());
-			preparedStatement.setString(3, customer.country());
-			preparedStatement.setString(4, customer.postal_code());
-			preparedStatement.setString(5, customer.phone());
-			preparedStatement.setString(6, customer.email());
-			preparedStatement.setInt(7, customer.customer_id());
-			rowAffected = preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return rowAffected;
-	}
-	
-	@Override
-	public String countCountry() {
-		String country = "";
-		String sql = "SELECT MAX(country) FROM customer";
-		
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			preparedStatement = conn.prepareStatement(sql);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			
-			while (resultSet.next()) {
-                country = resultSet.getString("max");
-            }
-            preparedStatement.close();
+  /* 6. Update an existing customer. */
+  @Override
+  public int update(Customer customer) {
+    String sql = "UPDATE customer SET first_name = ?, " +
+            "last_name = ?, " +
+            "country = ?, " +
+            "postal_code = ?, " +
+            "phone = ?, " +
+            "email = ? " +
+            "WHERE customer_id = ?";
+    int rowAffected = 0;
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
+      preparedStatement.setString(1, customer.first_name());
+      preparedStatement.setString(2, customer.last_name());
+      preparedStatement.setString(3, customer.country());
+      preparedStatement.setString(4, customer.postal_code());
+      preparedStatement.setString(5, customer.phone());
+      preparedStatement.setString(6, customer.email());
+      preparedStatement.setInt(7, customer.customer_id());
+      rowAffected = preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return rowAffected;
+  }
 
+  /* 7. Return the country with the most customers.*/
+  @Override
+  public String countCountry() {
+    String country = "";
+    String sql = "SELECT MAX(country) FROM customer";
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return country;
-	}
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      preparedStatement = conn.prepareStatement(sql);
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        country = resultSet.getString("max");
+      }
+      preparedStatement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return country;
+  }
 
-	@Override
-	public Customer highestSpender() {
-		Customer customer = null;
-		String sql = "SELECT invoice.invoice_id, customer.first_name, invoice.total, customer.customer_id, customer.last_name, customer.country, customer.postal_code, customer.phone, customer.email" +
-						"\tFROM invoice\n" +
-						"\tINNER JOIN customer\n" +
-						"\tON invoice.customer_id = customer.customer_id  \n" +
-						"\tWHERE total = (SELECT MAX(total) FROM invoice) ORDER BY total";
+  /* 8. Customer who is the highest spender (total in invoice table is the largest).*/
+  @Override
+  public Customer highestSpender() {
+    Customer customer = null;
+    String sql = "SELECT invoice.invoice_id, customer.first_name, invoice.total, customer.customer_id, customer.last_name, customer.country, customer.postal_code, customer.phone, customer.email" +
+            "\tFROM invoice\n" +
+            "\tINNER JOIN customer\n" +
+            "\tON invoice.customer_id = customer.customer_id  \n" +
+            "\tWHERE total = (SELECT MAX(total) FROM invoice) ORDER BY total";
 
-		try (Connection conn = DriverManager.getConnection(url, username, password)) {
-			PreparedStatement preparedStatement = conn.prepareStatement(sql);
-			ResultSet resultSet = preparedStatement.executeQuery();
+    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+      PreparedStatement preparedStatement = conn.prepareStatement(sql);
+      ResultSet resultSet = preparedStatement.executeQuery();
 
-			while (resultSet.next()) {
-				customer = new Customer(
-								resultSet.getInt("customer_id"),
-								resultSet.getString("first_name"),
-								resultSet.getString("last_name"),
-								resultSet.getString("country"),
-								resultSet.getString("postal_code"),
-								resultSet.getString("phone"),
-								resultSet.getString("email")
-				);
-			}
-			preparedStatement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return customer;
+      while (resultSet.next()) {
+        customer = new Customer(
+                resultSet.getInt("customer_id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("country"),
+                resultSet.getString("postal_code"),
+                resultSet.getString("phone"),
+                resultSet.getString("email")
+        );
+      }
+      preparedStatement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return customer;
 
-	}
+  }
 
+  @Override
+  public int delete(Customer object) {
+    return 0;
+  }
 
-
-
-	@Override
-	public int delete(Customer object) {
-		return 0;
-	}
-
-	@Override
-	public int deleteByID(Integer integer) {
-		return 0;
-	}
+  @Override
+  public int deleteByID(Integer integer) {
+    return 0;
+  }
 }
--- src/main/java/com/example/chinookjava/runners/CustomerRunner.java
+++ src/main/java/com/example/chinookjava/runners/CustomerRunner.java
@@ -6,48 +6,46 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class CustomerRunner implements ApplicationRunner {
-	private final CustomerRepository customerRepository;
+  private final CustomerRepository customerRepository;
 
-	public CustomerRunner(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
+  public CustomerRunner(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		/*Insert a new customer.*/
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    /*Insert a new customer.*/
 //		customerRepository.insert(new Customer(0, "Adrian", "Mattsson", "Sweden", "54134", "824674-23423", "adrian@mattsson.com"));
 
-		/*Update an existing customer*/
+    /*Update an existing customer*/
 //		customerRepository.update(new Customer(2, "Oliver", "Dahlqvist", "Sweden", "54144", "224256-2343", "oliver@dalhqvist.com"));
 
-		/*Prints out all customers.*/
-		System.out.println("\n_______________________ Find all customers. _______________________");
-		List<Customer> customerList = customerRepository.findAll();
-		customerList.forEach(customer -> System.out.println(customer.getCustomerInformation()));
+    /*Prints out all customers.*/
+    System.out.println("\n_______________________ Find all customers. _______________________");
+    List<Customer> customerList = customerRepository.findAll();
+    customerList.forEach(customer -> System.out.println(customer.getCustomerInformation()));
 
-		/*Prints out all customers with limit & offset.*/
-		System.out.println("\n_______________________ Find all customers with limits & offsets. _______________________");
-		List<Customer> customerListLimit = customerRepository.findAll(2, 12);
-		customerListLimit.forEach(customer -> System.out.println(customer.getCustomerInformation()));
+    /*Prints out all customers with limit & offset.*/
+    System.out.println("\n_______________________ Find all customers with limits & offsets. _______________________");
+    List<Customer> customerListLimit = customerRepository.findAll(2, 12);
+    customerListLimit.forEach(customer -> System.out.println(customer.getCustomerInformation()));
 
-		/*Prints out customer by first name*/
-		System.out.println("\n_______________________ Find customer by name. _______________________");
-		Customer customerName = customerRepository.findByName("Adrian");
-		System.out.println("\n" + customerName.getCustomerInformation());
-		
-		/*Prints out country with most customers*/
-		System.out.println("\n_______________________ Find country with most customers. _______________________");
-		System.out.println(customerRepository.countCountry());
+    /*Prints out customer by first name*/
+    System.out.println("\n_______________________ Find customer by name. _______________________");
+    Customer customerName = customerRepository.findByName("Adrian");
+    System.out.println("\n" + customerName.getCustomerInformation());
 
-		/*Prints out customer with the highest spender total on invoices.*/
-		System.out.println("\n_______________________ Find customer that spends most._______________________");
-		Customer customerSpender = customerRepository.highestSpender();
-		System.out.println(customerSpender.getCustomerInformation());
-	}
+    /*Prints out country with most customers*/
+    System.out.println("\n_______________________ Find country with most customers. _______________________");
+    System.out.println(customerRepository.countCountry());
+
+    /*Prints out customer with the highest spender total on invoices.*/
+    System.out.println("\n_______________________ Find customer that spends most._______________________");
+    Customer customerSpender = customerRepository.highestSpender();
+    System.out.println(customerSpender.getCustomerInformation());
+  }
 }
