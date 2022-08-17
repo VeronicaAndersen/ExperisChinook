8e9460562b804a39082336c9c4474f005e9a9213
Veronica Andersen <veronica.andersen@se.experis.com>
2022-08-17 09:14:52 +0200

    Refactor: Some refactoring on preparedstatement & resultSet to minimize code duplication.

--- src/main/java/com/example/chinookjava/repository/customer/CustomerRepositoryImpl.java
+++ src/main/java/com/example/chinookjava/repository/customer/CustomerRepositoryImpl.java
@@ -7,7 +7,6 @@ import com.example.chinookjava.models.CustomerSpender;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
-import javax.naming.Name;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,25 +31,12 @@ public class CustomerRepositoryImpl implements CustomerRepository {
   phone number and email.*/
   @Override
   public List<Customer> findAll() {
-    Customer customer = null;
     List<Customer> customerList = new ArrayList<>();
     String sql = "SELECT * FROM customer ORDER BY customer_id ASC";
     try (Connection conn = DriverManager.getConnection(url, username, password)) {
       preparedStatement = conn.prepareStatement(sql);
 
-      ResultSet resultSet = preparedStatement.executeQuery();
-      while (resultSet.next()) {
-        customer = new Customer(
-                resultSet.getInt("customer_id"),
-                resultSet.getString("first_name"),
-                resultSet.getString("last_name"),
-                resultSet.getString("country"),
-                resultSet.getString("postal_code"),
-                resultSet.getString("phone"),
-                resultSet.getString("email")
-        );
-        customerList.add(customer);
-      }
+      resultSet(customerList);
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -68,19 +54,7 @@ phone number and email. Whit limit & offsets*/
       preparedStatement = conn.prepareStatement(sql);
       preparedStatement.setInt(1, rowLimit);
       preparedStatement.setInt(2, offsetLimit);
-      ResultSet resultSet = preparedStatement.executeQuery();
-      while (resultSet.next()) {
-        customer = new Customer(
-                resultSet.getInt("customer_id"),
-                resultSet.getString("first_name"),
-                resultSet.getString("last_name"),
-                resultSet.getString("country"),
-                resultSet.getString("postal_code"),
-                resultSet.getString("phone"),
-                resultSet.getString("email")
-        );
-        customerList.add(customer);
-      }
+      resultSet(customerList);
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -117,51 +91,34 @@ phone number and email. Whit limit & offsets*/
   /* 3. Read a specific customer by name.*/
   @Override
   public List <Customer> findByName(String name) {
-    Customer customer = null;
     List<Customer> customerList = new ArrayList<>();
     String sql = "SELECT * FROM customer WHERE first_name LIKE ? ORDER BY customer_id ASC";
     try (Connection conn = DriverManager.getConnection(url, username, password)) {
       preparedStatement = conn.prepareStatement(sql);
       preparedStatement.setString(1, name);
-      ResultSet resultSet = preparedStatement.executeQuery();
-      while (resultSet.next()) {
-        customer = new Customer(
-                resultSet.getInt("customer_id"),
-                resultSet.getString("first_name"),
-                resultSet.getString("last_name"),
-                resultSet.getString("country"),
-                resultSet.getString("postal_code"),
-                resultSet.getString("phone"),
-                resultSet.getString("email")
-        );
-        customerList.add(customer);
-      }
+      resultSet(customerList);
     } catch (SQLException e) {
       e.printStackTrace();
     }
     return customerList;
   }
 
-  /* 5. Add a new customer to the database.*/
+
+
+  /* 5. Inserts a new customer to the database.*/
   @Override
   public void insert(Customer customer) {
     String sql = "INSERT INTO customer (first_name, last_name, country, postal_code, phone, email) VALUES (?,?,?,?,?,?)";
 
     try (Connection conn = DriverManager.getConnection(url, username, password)) {
-      preparedStatement = conn.prepareStatement(sql);
-      preparedStatement.setString(1, customer.first_name());
-      preparedStatement.setString(2, customer.last_name());
-      preparedStatement.setString(3, customer.country());
-      preparedStatement.setString(4, customer.postal_code());
-      preparedStatement.setString(5, customer.phone());
-      preparedStatement.setString(6, customer.email());
+      sqlConnection(customer, sql, conn);
       preparedStatement.executeUpdate();
     } catch (SQLException e) {
       e.printStackTrace();
     }
   }
 
-  /* 6. Update an existing customer. */
+  /* 6. Update an existing customer in the database. */
   @Override
   public int update(Customer customer) {
     String sql = "UPDATE customer SET first_name = ?, " +
@@ -173,13 +130,7 @@ phone number and email. Whit limit & offsets*/
             "WHERE customer_id = ?";
     int rowAffected = 0;
     try (Connection conn = DriverManager.getConnection(url, username, password)) {
-      preparedStatement = conn.prepareStatement(sql);
-      preparedStatement.setString(1, customer.first_name());
-      preparedStatement.setString(2, customer.last_name());
-      preparedStatement.setString(3, customer.country());
-      preparedStatement.setString(4, customer.postal_code());
-      preparedStatement.setString(5, customer.phone());
-      preparedStatement.setString(6, customer.email());
+      sqlConnection(customer, sql, conn);
       preparedStatement.setInt(7, customer.customer_id());
       rowAffected = preparedStatement.executeUpdate();
     } catch (SQLException e) {
@@ -212,19 +163,10 @@ phone number and email. Whit limit & offsets*/
   @Override
   public CustomerSpender customerThatSpendsMost() {
     CustomerSpender spender = null;
-    String sql = "SELECT invoice.invoice_id, " +
-            "customer.first_name, " +
-            "invoice.total, " +
-            "customer.customer_id, " +
-            "customer.last_name, " +
-            "customer.country, " +
-            "customer.postal_code, " +
-            "customer.phone, " +
-            "customer.email" +
-            "\tFROM invoice\n" +
-            "\tINNER JOIN customer\n" +
-            "\tON invoice.customer_id = customer.customer_id  \n" +
-            "\tWHERE total = (SELECT MAX(total) FROM invoice) ORDER BY total";
+    String sql = "SELECT invoice.invoice_id, customer.first_name, invoice.total, customer.customer_id, " +
+            "customer.last_name, customer.country, customer.postal_code, customer.phone, customer.email " +
+            "FROM invoice INNER JOIN customer\n ON invoice.customer_id = customer.customer_id  " +
+            "WHERE total = (SELECT MAX(total) FROM invoice) ORDER BY total";
 
     try (Connection conn = DriverManager.getConnection(url, username, password)) {
       PreparedStatement preparedStatement = conn.prepareStatement(sql);
@@ -243,7 +185,7 @@ phone number and email. Whit limit & offsets*/
     }
     return spender;
   }
-/*For a given customer, their most popular genre (in the case of a tie, display both). Most popular in this context
+/* 9. For a given customer, their most popular genre (in the case of a tie, display both). Most popular in this context
 means the genre that corresponds to the most tracks from invoices associated to that customer.*/
   @Override
   public List<CustomerGenre> customerGreatestGenre(Customer customer) {
@@ -277,5 +219,44 @@ means the genre that corresponds to the most tracks from invoices associated to 
       e.printStackTrace();
     }
     return genre;
+  }
+
+  /**
+   * Method for result Set with SQL Exceptions.
+   * @param customerList sets data from sql query into list.
+   * @throws SQLException throws error if error occurs.
+   */
+  private void resultSet(List<Customer> customerList) throws SQLException {
+    Customer customer;
+    ResultSet resultSet = preparedStatement.executeQuery();
+    while (resultSet.next()) {
+      customer = new Customer(
+              resultSet.getInt("customer_id"),
+              resultSet.getString("first_name"),
+              resultSet.getString("last_name"),
+              resultSet.getString("country"),
+              resultSet.getString("postal_code"),
+              resultSet.getString("phone"),
+              resultSet.getString("email")
+      );
+      customerList.add(customer);
+    }
+  }
+
+  /**
+   * Method for sql connection with SQL Exceptions.
+   * @param customer sets a value to sql query that will be executed.
+   * @param sql takes the sql query that will be executed.
+   * @param conn the connection between the application and database.
+   * @throws SQLException throws error if error occurs.
+   */
+  private void sqlConnection(Customer customer, String sql, Connection conn) throws SQLException {
+    preparedStatement = conn.prepareStatement(sql);
+    preparedStatement.setString(1, customer.first_name());
+    preparedStatement.setString(2, customer.last_name());
+    preparedStatement.setString(3, customer.country());
+    preparedStatement.setString(4, customer.postal_code());
+    preparedStatement.setString(5, customer.phone());
+    preparedStatement.setString(6, customer.email());
   }
 }
